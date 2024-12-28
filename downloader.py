c10fa689ba6ab8300f7561ad6aff074d9d9ca92f
barbixxxa <13052258+barbixxxa@users.noreply.github.com>
2024-12-27 23:16:55 -0300

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -4,9 +4,9 @@ import requests
 import argparse
 import os
 from bs4 import BeautifulSoup
+from datetime import datetime
 import csv
 import xml.etree.ElementTree as ET
-from datetime import datetime
 
 requests.packages.urllib3.disable_warnings()
 requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS += ':HIGH:!DH:!aNULL'
@@ -18,7 +18,9 @@ parser.add_argument("--xml", help="Arquivo XML para converter em CSV")
 parser.add_argument("--csv", help="Arquivo de saída CSV")
 args = parser.parse_args()
 
-def limparString(string):
+NAMESPACE = './/{http://www.portalfiscal.inf.br/nfe}'
+
+def limpar_string(string):
     return string.replace(" ", "_").replace(".", "_").replace("/", "_").replace("\\", "_").lower()
 
 def remover_espacos(string):
@@ -34,32 +36,32 @@ def formatar_data(data_str):
 def substituir_pontos_por_virgulas(valor):
     return valor.replace('.', ',')
 
+def salvar_nfce(arquivo_conteudo_xml, arquivo_dir='output'):
+    xNome = limpar_string(arquivo_conteudo_xml.find(f'{NAMESPACE}xNome').text)
+    dhRecbto = limpar_string(arquivo_conteudo_xml.find(f'{NAMESPACE}dhRecbto').text.split('T')[0])
+    nProt = limpar_string(arquivo_conteudo_xml.find(f'{NAMESPACE}nProt').text)
+
+    arquivo_nome = f'nfce-{xNome}-{dhRecbto}-{nProt}.xml'
+    arquivo_output_path = os.path.join(arquivo_dir, arquivo_nome)
+
+    if not os.path.exists(arquivo_dir):
+        os.mkdir(arquivo_dir)
+        print(f'=> Pasta NÃO encontrada!\n==> Criando o diretório: {arquivo_dir}\n')
+
+    if os.path.exists(arquivo_output_path):
+        print(f'=> Arquivo já existe: {arquivo_output_path}')
+        return
+
+    with open(arquivo_output_path, 'w') as arquivo_output:
+        arquivo_output.write(arquivo_conteudo_xml.prettify())
+        print(f'=> NFCE salva com sucesso!\n==> ARQUIVO: \'{arquivo_nome}\'\n==> PASTA: \'{arquivo_dir}\'')
+
 def processar_url(url, erros):
     try:
         response = requests.get(url, verify=False)
         arquivo_conteudo_raw = response.text
-
         arquivo_conteudo_xml = BeautifulSoup(arquivo_conteudo_raw, "xml")
-
-        xNome = limparString(arquivo_conteudo_xml.find('xNome').text)
-        dhRecbto = limparString(arquivo_conteudo_xml.find('dhRecbto').text.split('T')[0])
-        nProt = limparString(arquivo_conteudo_xml.find('nProt').text)
-
-        arquivo_nome = 'nfce-' + xNome + '-' + dhRecbto + '-' + nProt + '.xml'
-        arquivo_dir = 'output'
-        arquivo_output_path = os.path.join(arquivo_dir, arquivo_nome)
-
-        if not os.path.exists(arquivo_dir):
-            os.mkdir(arquivo_dir)
-            print('=> Pasta NÃO encontrada!\n==> Criando o diretório: ' + arquivo_dir + '\n')
-
-        if os.path.exists(arquivo_output_path):
-            print('=> Arquivo já existe: ' + arquivo_output_path)
-            return
-
-        with open(arquivo_output_path, 'w') as arquivo_output:
-            arquivo_output.write(arquivo_conteudo_xml.prettify())
-            print('=> NFCE salva com sucesso!\n==> ARQUIVO: \'' + arquivo_nome + '\'\n==> PASTA: \'' + arquivo_dir + '\'')
+        salvar_nfce(arquivo_conteudo_xml)
     except Exception as e:
         print(f'[ERROR] Falha ao processar URL {url}')
         print(e)
@@ -69,32 +71,27 @@ def xml_to_csv(xml_file, csv_file):
     try:
         tree = ET.parse(xml_file)
         root = tree.getroot()
-
-        # Find the infNFe node
-        infNFe = root.find('.//{http://www.portalfiscal.inf.br/nfe}infNFe')
+        infNFe = root.find(f'{NAMESPACE}infNFe')
 
         if infNFe is None:
             raise ValueError("Tag infNFe não encontrada no arquivo XML.")
 
         with open(csv_file, mode='w', newline='') as file:
             writer = csv.writer(file)
-            
-            # Write the headers
             header = ["dhEmi", "cProd", "xProd", "qCom", "vUnCom", "vProd", "uTrib"]
             writer.writerow(header)
-            
-            # Extract data and write to CSV
-            dhEmi_elem = infNFe.find('.//{http://www.portalfiscal.inf.br/nfe}dhEmi')
+
+            dhEmi_elem = infNFe.find(f'{NAMESPACE}dhEmi')
             dhEmi = formatar_data(remover_espacos(dhEmi_elem.text)) if dhEmi_elem is not None else ''
-            for det in infNFe.findall('.//{http://www.portalfiscal.inf.br/nfe}det'):
-                prod = det.find('.//{http://www.portalfiscal.inf.br/nfe}prod')
+            for det in infNFe.findall(f'{NAMESPACE}det'):
+                prod = det.find(f'{NAMESPACE}prod')
                 if prod is not None:
-                    cProd_elem = prod.find('.//{http://www.portalfiscal.inf.br/nfe}cProd')
-                    xProd_elem = prod.find('.//{http://www.portalfiscal.inf.br/nfe}xProd')
-                    qCom_elem = prod.find('.//{http://www.portalfiscal.inf.br/nfe}qCom')
-                    vUnCom_elem = prod.find('.//{http://www.portalfiscal.inf.br/nfe}vUnCom')
-                    vProd_elem = prod.find('.//{http://www.portalfiscal.inf.br/nfe}vProd')
-                    uTrib_elem = prod.find('.//{http://www.portalfiscal.inf.br/nfe}uTrib')
+                    cProd_elem = prod.find(f'{NAMESPACE}cProd')
+                    xProd_elem = prod.find(f'{NAMESPACE}xProd')
+                    qCom_elem = prod.find(f'{NAMESPACE}qCom')
+                    vUnCom_elem = prod.find(f'{NAMESPACE}vUnCom')
+                    vProd_elem = prod.find(f'{NAMESPACE}vProd')
+                    uTrib_elem = prod.find(f'{NAMESPACE}uTrib')
                     cProd = remover_espacos(cProd_elem.text) if cProd_elem is not None else ''
                     xProd = remover_espacos(xProd_elem.text) if xProd_elem is not None else ''
                     qCom = remover_espacos(qCom_elem.text) if qCom_elem is not None else ''
@@ -110,7 +107,6 @@ def xml_to_csv(xml_file, csv_file):
 
 def main():
     erros = []
-    
     if args.url:
         processar_url(args.url, erros)
     elif args.file:
@@ -127,7 +123,7 @@ def main():
         xml_to_csv(args.xml, args.csv)
     else:
         print("Por favor, forneça uma URL, um arquivo txt com URLs, ou um arquivo XML e um arquivo CSV para saída.")
-    
+
     if erros:
         with open('erros.txt', 'w') as erro_file:
             for url in erros:
